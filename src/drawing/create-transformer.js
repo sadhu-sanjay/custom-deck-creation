6d462af2eb642b37c83d58ba2c14d1a7eb917c3b
sadhu-sanjay <sanjaygoswami60@gmail.com>
2024-04-15 21:54:49 +0530

    rotationon working

---
+++ src/drawing/create-transformer.js
@@ -0,0 +1,35 @@
+export function createTransformer(stage) {
+
+  const tr = new Konva.Transformer({
+    keepRatio: true,
+    rotationSnaps: [0,45,90, 135,180,225, 270, 360],
+    enabledAnchors: ["rotater", "top-center", "bottom-center"],
+    anchorStyleFunc: (anchor) => {
+
+      if (anchor.hasName("rotater")) {
+        const patternImage = new Image();
+        patternImage.src = "/refresh.png";
+
+        anchor.fillPatternImage(patternImage);
+        anchor.height(16);
+        anchor.width(16);
+        anchor.strokeWidth(0)
+        anchor.offsetX(8);
+        anchor.offsetY(8);
+
+        anchor.fill(null);
+      }
+    },
+  });
+
+  const rotater = tr.findOne(".rotater");
+  rotater.on("mouseenter", () => {
+    stage.content.style.cursor = "url(/refresh.png), auto";
+  });
+
+  rotater.on("mouseleave", () => {
+    stage.content.style.cursor = "";
+  });
+
+  return tr
+}
